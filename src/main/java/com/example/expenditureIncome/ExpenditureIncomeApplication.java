6c4a5d4fe65f61ec1bb89010c54c485c804bea48
yoko-Developer <whitesnow.wave@gmail.com>
2024-11-27 22:06:16 +0900

    initial commit

---
+++ src/main/java/com/example/expenditureIncome/ExpenditureIncomeApplication.java
@@ -0,0 +1,13 @@
+package com.example.expenditureIncome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenditureIncomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenditureIncomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/expenditureIncome/ExpenditureIncomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expenditureIncome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenditureIncomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
